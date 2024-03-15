da373ded236cf9e8c7a2e3411924dd5d82b15a65
adhilario1 <adam.hilario@outlook.com>
2024-03-15 15:48:35 +0000

    edited game page

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -8,7 +8,11 @@ import config from '@/amplifyconfiguration.json';
 import Header from '@/components/Header';
 import Body from '@/components/Body';
 import Footer from '@/components/Footer';
+import ImageCarousel from '@/components/ImageCarousel';
 //import { Games } from '@/API';
+
+//style
+import '@/css/GamePage.css'
 
 const cookiesClient = generateServerClientUsingCookies({
   config,
@@ -36,14 +40,36 @@ export default async function Page({ params }: { params: { id: string } }) {
           </div>
         )} 
         {(game) && (
-          <div>
-            {game.title}
-            {game.description}
-            <iframe frameBorder="0" src={game.url} width="552" height="167">
-              {(game.outbound_url) && (
-                <a href={game.outbound_url}>{game.outbound_label}</a>
+          <div className='content game'>
+            <div className='game-header'>
+              <h2 className='secondary-label'>{game.title}</h2>
+              <h3 className='tertiary-label-normal'>{game?.year}</h3>
+            </div>
+            
+            <div className='game-preview'>
+              {(game?.preview && game?.preview.length > 0 ) && (
+                <>
+                <ImageCarousel images={game.preview} />
+                </>
               )}
-            </iframe>
+            </div>
+            
+            <div className='description'>
+              {game.description}
+            </div>
+            
+            <div className='game-link'> 
+              {(game?.outbound_url !== undefined || game?.outbound_url !== "") && (
+                <div className='outbound'> 
+                  <h2 className='tertiary-label-bold'>Play Me!</h2>
+                  <iframe frameBorder="0" src={game.url} width="552" height="167">
+                  {(game.outbound_url) && (
+                    <a href={game.outbound_url}>{game.outbound_label}</a>
+                  )}
+                  </iframe>
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
