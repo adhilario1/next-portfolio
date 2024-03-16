410cd79c20984884067bec3b4b3779b03f9e4cda
adhilario1 <44608445+adhilario1@users.noreply.github.com>
2024-03-16 18:19:42 +0000

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,6 @@ import Document, {
             <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
           </Head>
           <body>
-            <div className='background'></div>
             <Main />
             <NextScript />
           </body>
