e4982aff3981eb8a93478a2a8d99c2d58822a588
adhilario1 <adam.hilario@outlook.com>
2024-03-17 14:33:37 +0000

    email functionality

--- src/pages/contact/email.tsx
+++ src/pages/contact/email.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '@/components/Header';
 import Body from '@/components/Body';
 import Footer from '@/components/Footer';
 
 import '@/css/email.css'
-
-interface FormProps{
-    onSubmit: (data: FormData) => void;
-}
 
 interface FormData {
     name: string;
@@ -20,9 +16,9 @@ interface FormData {
 
 const Email = () => {
     const [formData, setFormData] = React.useState<FormData>({ name: '', email: '', message: '' });
-
+    const [sent, setSentStatus] = useState(false);
     //lambda
-
+    const endpoint = 'https://njzdm5huxbtycvrsnydjfpaiwy0rttcn.lambda-url.eu-west-1.on.aws/';
 
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
         const {name, value} = event.target;
@@ -34,7 +30,7 @@ const Email = () => {
         setFormData({ ...formData, [name]: value});
     }
 
-    /*
+    
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const data = formData;
@@ -42,7 +38,7 @@ const Email = () => {
         const fetchPromise = fetch(endpoint, {
             method: 'POST',
             mode: 'cors',
-            cache: 'no-cahce',
+            cache: 'no-cache',
             body: JSON.stringify(data)
         });
 
@@ -50,45 +46,52 @@ const Email = () => {
             .then(response => response.json())
             .then(data => {
                 console.log(data); //handles respons, catch errors
-            })
+            });
+        
+        setSentStatus(true);
     }
-    */
-
-    const handleSubmit = () => {
-
-    }
+    
     return (
         <>
         <Header/>
         <Body>
             <div className='content'>
-                <div className='contact-form'>
-                    <h1 className='secondary-label'>contact me.</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div className='top'>
-                            <div className='field half'>
-                                <label htmlFor='name' >Name:</label>
-                                <input name="name" type="text" value={formData.name} onChange={handleInputChange}/><br/><br/>
+                {(!sent) && (                    
+                    <div className='contact-form'>
+                        <h1 className='secondary-label'>contact me.</h1>
+                        <form action={endpoint} onSubmit={handleSubmit} method='POST'>
+                            <div className='top'>
+                                <div className='field half'>
+                                    <label htmlFor='name' >Name:</label>
+                                    <input name="name" type="text" value={formData.name} onChange={handleInputChange} required/><br/><br/>
+                                </div>
+                                <div className='field half'>
+                                    <label htmlFor="email">Email:</label>
+                                    <input name="email" type="email" value={formData.email} onChange={handleInputChange} required/><br/><br/>
+                                </div>
                             </div>
-                            <div className='field half'>
-                                <label htmlFor="email">Email:</label>
-                                <input name="email" type="email" value={formData.email} onChange={handleInputChange}/><br/><br/>
+                            <div className='field'>
+                                <label htmlFor='subject'>Subject:</label>
+                                <input id='subject-line' name="subject" type="text" value={formData.subject} onChange={handleInputChange} required/><br/><br/>
                             </div>
-                        </div>
-                        <div className='field'>
-                                <label htmlFor='subject' >Subject:</label>
-                                <input id='subject-line' name="subject" type="text" value={formData.subject} onChange={handleInputChange}/><br/><br/>
+                            <div className='field'>
+                                <label htmlFor="Message">Message:</label>
+                                <textarea name="message" value={formData.message} onChange={handleMessageChange} required></textarea><br/><br/>
                             </div>
-                        <div className='field'>
-                            <label htmlFor="Message">Message:</label>
-                            <textarea name="message" value={formData.message} onChange={handleMessageChange}></textarea><br/><br/>
-                        </div>
-                        <input type='submit'/>
-                        <div>
-                            <p id="result-text"></p>
-                        </div>
-                    </form>
-                </div>
+                            <input type='submit'/>
+                            <div>
+                                <p id="result-text"></p>
+                            </div>
+                        </form>
+                    </div>
+                )}
+                {sent && (
+                    <div className='confirmation'>
+                        <h1 className=''>Thank You for Reaching Out!</h1>
+                        <p>I will respond as soon as I can!</p>
+                        {<img src='/assets/images/IMG_2106.jpg' />}
+                    </div>
+                )}
             </div>
         </ Body>
         <Footer />
